e8e2ee44eab5db434ae51091c6689dcffe65940f
samuelfyneweverucsp <samuel.fynewever@ucsp.edu.pe>
2022-09-01 08:18:13 -0500

    sumandoEnteros

---
+++ semana2/sumandoEnteros.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int a = 20;
+    int b = 10;
+
+    int suma = a + b;
+
+    cout << "La suma es: " << suma << endl;
+
+    return 0;
+}
