c80402a3a5e21724008efdf020185d0fa05751d4
sbf5calvin <112088637+samuelfyneweverucsp@users.noreply.github.com>
2022-09-05 22:42:54 -0500

    Update 2-32.cpp
    Co-Authored-By: samuelfyneweverucsp <112088637+samuelfyneweverucsp@users.noreply.github.com>

--- semana2/2-32.cpp
+++ semana2/2-32.cpp
@@ -11,6 +11,7 @@ int main() {
     float MHR1{0};
     float MHR2{0};
     float MHR3{0};
+    float MHR4{0};
 
     float mayor{0};
     float menor{0};
@@ -20,16 +21,19 @@ int main() {
 
     MHR1 = 220 - edad;
     MHR2 = 208 - 0.7 * edad;
-    MHR3 = 211 - 0.64 * edad;
+    MHR3 = MHR2 - 1;
+    MHR4 = 211 - 0.64 * edad;
 
     cout << "MHR1: " << MHR1 << "\nMHR2: " << MHR2 << "\nMHR3: " << MHR3 << endl;
-    if ((MHR1 > MHR2) && (MHR1 > MHR3)) {mayor = MHR1;}
-    else if (MHR2 > MHR3) {mayor = MHR2;}
-    else {mayor = MHR3;}
+    if ((MHR1 > MHR2) && (MHR1 > MHR3) && (MHR1 > MHR4)) {mayor = MHR1;}
+    else if ((MHR2 > MHR3) && (MHR2 > MHR4)) {mayor = MHR2;}
+    else if (MHR3 > MHR4) {mayor = MHR3;}
+    else {mayor = MHR4;}
 
-    if ((MHR1 < MHR2) && (MHR1 < MHR3)) {menor = MHR1;}
-    else if (MHR2 < MHR3) {menor = MHR2;}
-    else {menor = MHR3;}
+    if ((MHR1 < MHR2) && (MHR1 < MHR3) && (MHR1 < MHR4)) {menor = MHR1;}
+    else if ((MHR2 < MHR3) && (MHR2 < MHR4)) {menor = MHR2;}
+    else if (MHR3 < MHR4) {menor = MHR3;}
+    else {menor = MHR4;}
     cout << "Rango de MHRs: (" << menor << ", " << mayor << ")" << endl;
 
     
