1bcf84f474bd8665d2075e73887f70b75315a0af
samuelfyneweverucsp <samuel.fynewever@ucsp.edu.pe>
2022-09-08 08:24:33 -0500

    account class de cap 3

---
+++ semana3/AccountTest.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file AccountTest.cpp
+ * @author samuelfyneweverucsp (samuel.fynewever@ucsp.edu.pe)
+ * @brief creating and manipulating a (bank) Account object
+ * @version 0.1
+ * @date 2022-09-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <string>
+
+#include "Account.h"
+
+using namespace std;
+
+int main () {
+    Account account1{"Rigoberto", 100};
+    Account account2{"Nicolas", 50};
+
+    cout << "account1: " << account1.getName() << " tiene balance de S/" << account1.getBalance(); 
+    cout << "\naccount2: " << account2.getName() << " tiene balance de S/" << account2.getBalance();
+
+    cout << "\n\nIngresa cuanto depositar en account1: " << endl;
+    int depositAmount;
+    cin >> depositAmount;
+    cout << "Agregando S/" << depositAmount << " al balance de account1...";
+    account1.deposit(depositAmount); // agregar al balance de account1
+
+    // mostrar los balances de nuevo
+    cout << "\n\naccount1: " << account1.getName() << " tiene balance de S/" << account1.getBalance(); 
+    cout << "\naccount2: " << account2.getName() << " tiene balance de S/" << account2.getBalance();
+
+    cout << "\n\nIngresa cuanto depositar en account2: " << endl;
+    cin >> depositAmount;
+    cout << "agregando " << depositAmount << "al balance de account2...";
+    account2.deposit(depositAmount); // agregar al balance de account2
+
+    // mostrar los balances una vez mas
+    cout << "\n\naccount1: " << account1.getName() << " tiene balance de S/" << account1.getBalance(); 
+    cout << "\naccount2: " << account2.getName() << " tiene balance de S/" << account2.getBalance();    
+}
---
+++ semana3/account.h
@@ -0,0 +1,55 @@
+/**
+ * @file account.h
+ * @author samuelfyneweverucsp (samuel.fynewever@ucsp.edu.pe)
+ * @brief clase Account, con constructor para inicalizar el nombre de la cuenta
+ * @version 0.1
+ * @date 2022-09-08
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+
+using std::string;
+using std::cout;
+
+
+class Account {
+    public:
+        explicit Account(std::string accountName, int initialBalance)
+        : nameDataMember{accountName} {
+            // validar que initialBalance es positivo; si no lo es,
+            // mantener el balance en 0
+            if (initialBalance > 0) {
+                balanceDataMember = initialBalance;
+            }
+        }
+
+        // funcion que deposita solamente una cantidad valida al balance
+        void deposit(int depositAmount) {
+            if (depositAmount > 0) { // si la cantidad para depositar es valido
+                balanceDataMember = balanceDataMember + depositAmount; // agregarlo al balance
+            }
+        }
+
+        // funcion que retorna el balance del cuenta
+        int getBalance() const {
+            return balanceDataMember;
+        }
+
+        // establecer nombre
+        void setName(std::string accountName) {
+            nameDataMember = accountName;
+        }
+
+        // retornar el nombre
+        std::string getName() const {
+            return nameDataMember;
+        }
+
+    private:
+        std::string nameDataMember;
+        int balanceDataMember{0};
+};
+
