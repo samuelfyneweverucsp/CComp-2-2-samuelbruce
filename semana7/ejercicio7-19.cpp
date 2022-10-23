8ec4e76636190ab2b414b478da83ea12df7753f5
sbf5calvin <112088637+samuelfyneweverucsp@users.noreply.github.com>
2022-10-22 22:12:45 -0500

    Ejercicios del laboratorio de la semana pasada

--- semana7/ejercicio7-19.cpp
+++ semana7/ejercicio7-19.cpp
@@ -1,5 +1,8 @@
 // Fig. 7.21: fig07_21.cpp
 // Demonstrating C++ Standard Library class template vector.
+// Ejercicio 7.19: reducirlo a arreglos,
+// descartando todo que un VECTOR puede hacer, pero un ARRAY no puede. 
+
 #include <iostream>
 #include <iomanip>
 #include <vector>
@@ -9,66 +12,69 @@
 using namespace std;
 using std::cout;
 
-int tam = 0;
+size_t tam = 0;
 
-void outputArray(const array<int, tam>&); // display the vector
-void inputArray(array<int, tam>&); // input values into the vector
+void outputArray(int arreglo[]); // display the vector
+void inputArray(int arreglo[]); // input values into the vector
 
 int main() {
-    array<int, 7> integers1; // 7-element array<int>
-    array<int, 10> integers2; // 10-element array<int>
+    int integers1[7]; // 7-element array<int>
+    int integers2[10]; // 10-element array<int>
 
     // print integers1 size and contents
-    cout << "Size of vector integers1 is " << integers1.size()
+    cout << "Size of array integers1 is " << sizeof(integers1)/sizeof(integers1[0])
     << "\nvector after initialization:";
-    outputVector(integers1);
+    outputArray(integers1);
 
     // print integers2 size and contents
-    cout << "\nSize of vector integers2 is " << integers2.size()
+    cout << "\nSize of array integers2 is " << sizeof(integers2)/sizeof(integers2[0])
     << "\nvector after initialization:";
-    outputVector(integers2);
+    outputArray(integers2);
 
     // input and print integers1 and integers2
     cout << "\nEnter 17 integers:" << endl;
-    inputVector(integers1);
-    inputVector(integers2);
+    inputArray(integers1);
+    inputArray(integers2);
 
-    cout << "\nAfter input, the vectors contain:\n"
+    cout << "\nAfter input, the arrays contain:\n"
     << "integers1:";
-    outputVector(integers1);
+    outputArray(integers1);
     cout << "integers2:";
-    outputVector(integers2);
+    outputArray(integers2);
 
-    // use inequality (!=) operator with vector objects
-    cout << "\nEvaluating: integers1 != integers2" << endl;
+    // use inequality (!=) operator with vector objects: no se puede
+    /*cout << "\nEvaluating: integers1 != integers2" << endl;
 
     if (integers1 != integers2) {
         cout << "integers1 and integers2 are not equal" << endl;
     }
-
+    */
     // create vector integers3 using integers1 as an
-    // initializer; print size and contents
+    // initializer; print size and contents - no se puede con arrays
 
-    vector<int> integers3{integers1}; // copy constructor
+    // vector<int> integers3{integers1}; // copy constructor — no se existe con ARRAYS
 
-    cout << "\nSize of vector integers3 is " << integers3.size()
+    /* cout << "\nSize of vector integers3 is " << integers3.size()
     << "\nvector after initialization: ";
     outputVector(integers3);
-
-    // use overloaded assignment (=) operator
+    */ 
+    // use overloaded assignment (=) operator with ARRAYS: no se puede 
+    /*
     cout << "\nAssigning integers2 to integers1:" << endl;
     integers1 = integers2; // assign integers2 to integers1
-
+    */
     cout << "integers1: ";
-    outputVector(integers1);
+    outputArray(integers1);
     cout << "integers2: ";
-    outputVector(integers2);
+    outputArray(integers2);
 
-    // use equality (==) operator with vector objects
+    // use equality (==) operator with arrays objects: no se debe
+    /*
     cout << "\nEvaluating: integers1 == integers2" << endl;
     if (integers1 == integers2) {
         cout << "integers1 and integers2 are equal" << endl;
     }
+    */
 
      // use square brackets to use the value at location 5 as an rvalue
     cout << "\nintegers1[5] is " << integers1[5];
@@ -78,36 +84,40 @@ int main() {
     integers1[5] = 1000;
 
     cout << "integers1: ";
-    outputVector(integers1);
+    outputArray(integers1);
 
-    // attempt to use out-of-range subscript
+    // attempt to use out-of-range subscript: no va a funcionar; o sea, va a retornar valores basuros
     try {
      cout << "\nAttempt to display integers1.at(15)" << endl;
-     cout << integers1.at(15) << endl; // ERROR: out of range
+     cout << integers1[15] << endl; // no hay ERROR: out of range -- va a retornar un valor basuro
     }
     catch (out_of_range& ex) {
      cerr << "An exception occurred: " << ex.what() << endl;
     } 
 
-    // changing the size of a vector
+    // changing the size of an ARRAY -- tampoco se puede
+    /*
     cout << "\nCurrent integers3 size is: " << integers3.size() << endl;
     integers3.push_back(1000); // add 1000 to the end of the vector
     cout << "New integers3 size is: " << integers3.size() << endl;
     cout << "integers3 now contains: ";
     outputVector(integers3);
+    */
 }
 
     // output vector contents
-void outputVector(const vector<int> & items) {
-    for (int item : items) {
-        cout << item << " ";
+void outputArray(int arreglo[]) {
+    int tam = sizeof(arreglo)/sizeof(arreglo[0]);
+    for(int i = 0; i <= tam; i++) {
+        cout << arreglo[i] << " ";
     }
     cout << endl;
 }
 
 // input vector contents
-void inputVector(vector<int>& items) {
-    for (int& item : items) {
-        cin >> item;
+void inputArray(int arreglo[]) {
+    int tam = sizeof(arreglo)/sizeof(arreglo[0]);
+    for (int i = 0; i <= tam; i++) {
+        cin >> arreglo[i];
     }
 } 
--- semana7/ejercicio7-30.cpp
+++ semana7/ejercicio7-30.cpp
@@ -1,5 +1,29 @@
 /*
-7.30 (Print an array) Write a recursive function printArray that takes an array, a starting subscript and an ending subscript as arguments, returns nothing and prints the array. The function
+7.30 (Print an array) Write a recursive function printArray that takes an array, 
+a starting subscript and an ending subscript as arguments, 
+returns nothing and prints the array. The function
 should stop processing and return when the starting subscript equals the ending subscript.
 */
 
+#include <iostream>
+#include <iomanip>
+#include <array>
+
+using namespace std;
+
+void printArray(int arreglo[], int subBegin, int subEnd);
+
+int main () {
+    int miArreglo[] = {1, 2, 3, 4, 5};
+    int miPrincipio = 2;
+    int miFin = 4;
+    printArray(miArreglo, miPrincipio, miFin);
+}
+
+void printArray(int arreglo[], int subBegin, int subEnd) {
+    for (int i = subBegin; i <= subEnd; i++) {
+        cout << arreglo[i] << " " << endl;
+    }
+}
+
+// g++ -std=c++11 FILENAME
